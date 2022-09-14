88238b93b7b092aa2a3fd0a406b58abff435c1a6
greenlemonkp <greenlemonkp@gmail.com>
2022-09-14 12:29:01 -0400

    secondTry

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,6 +1,5 @@
 let pets = ['cat', 'dog', 'rat'];
-for (let i = 0; i < pets.length; i ++) {
+  for(let i = 0; i < pets.length; i++) {
     pets[i] = pets[i] + 's'
-}
-
-console.log(pets)
+  }
+  console.log(pets);
